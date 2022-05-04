387f3bc6d4a1b621d6851fe4011b416f605c7581
AliceColombari <colombari.23@gmail.com>
2022-05-04 12:09:39 +0200

    creata prima parte base generale crud home

---
+++ app/Http/Controllers/User/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $user = Auth::user();
+        return view('user.home', compact('user'));
+    }
+}
