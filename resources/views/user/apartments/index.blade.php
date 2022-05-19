d8b65cddc73029d2fbdfd6f4e6fe61426feacc3c
Marco Sandri <sandrimarco98@gmail.com>
2022-05-19 19:15:47 +0200

    modali

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -2,6 +2,39 @@
 
 @section('content')
     <div class="container">
+
+        <div id="showMessage" class="messages">
+            <span onclick="document.getElementById('showMessage').style.display='none'" class="exit" title="Close Modal">×</span>
+            <div class="holes"></div>
+            <div class="dashed"></div>
+
+                <div class="messagesBox">
+                    @foreach ($apartments as $apartment)
+                        @if (count($apartment->messages) != 0)
+                        
+                            <div class="singleApartmentMessages">
+                                <div class="text-left pl-2 mb-2">
+                                    <h3 class="title"><a href="{{route('user.apartments.show', $apartment->slug)}}">{{$apartment->title}}</a></h3>
+                                </div>
+                                @foreach ($apartment->messages as $message)
+                                    <div class="singleMessage">
+                                        <div class="info pl-2">
+                                            <div>{{$message->email}}</div>
+                                            <div class="timeStamp">{{$message->created_at}}</div>
+                                        </div>
+                                        <div class="contentMessage">
+                                            {{$message->content}}
+                                        </div>
+                                    </div>
+
+                                @endforeach
+                            </div>
+                        @endif
+
+                    @endforeach
+                </div>
+        </div>
+
         <div class="row justify-content-center">
             <div class="col-md-12">
 
@@ -45,36 +78,47 @@
                                     <hr> 
                                     <span class="descrizione_prova">{{$apartment->description}}</span>
                                 </td>
-                                {{-- <td>{{$apartment->rooms_number}}</td>
-                                <td>{{$apartment->bathrooms_number}}</td>
-                                <td>{{$apartment->beds_number}}</td>
-                                <td>{{$apartment->square_meters}}</td>
-                                <td>{{$apartment->latitude}}</td>
-                                <td>{{$apartment->longitude}}</td> --}}
-                                {{-- <td>{{$apartment->visibility}}</td>
-                                <td>{{$apartment->city}}</td>
-                                <td>{{$apartment->address}}</td>
-                                <td>{{$apartment->zip_code}}</td> --}}
                                 <td class="align-middle">
                                     <div class="d-flex justify-content-around align-items-center">
                                         <a href="{{route('user.apartments.show', $apartment->slug)}}"><img src="http://127.0.0.1:8000/storage/icons/normal/vedi.svg" alt="mostra" class="svg_dash"></a>
                                         <a href="{{route('user.apartments.edit', $apartment->slug)}}"><img src="http://127.0.0.1:8000/storage/icons/normal/modifica.svg" alt="mostra" class="svg_dash"></a>
-    
-                                        <form method="POST" action="{{route('user.apartments.destroy', $apartment->id)}}">
+                                        <a class="btn ms_delete m-2" type="button" value="Elimina Appartamento" data-toggle="modal" data-target="#ModalDelete{{$apartment->id}}"><img src="http://127.0.0.1:8000/storage/icons/normal/cancella.svg" alt="elimina" class="svg_dash"></a>
+
+
+                                        
+                                        <form action="{{route("user.apartments.destroy", $apartment->id)}}" method="POST">
+                                            @csrf
+                                            @method("DELETE")
+                                            
+                                            <div class="modal fade" id="ModalDelete{{$apartment->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <img src="http://127.0.0.1:8000/storage/elements/bust-53.svg" class="imageDelete" alt="omino">
+                                                        <h1 class="text-center">Sei sicuro di voler eliminare {{$apartment->title}}?</h1>
+                                                        <div class="modalButtons">
+                                                            <button type="button" class="annulla" data-dismiss="modal">Chiudi</button>
+                                                            <input type="submit" class="cancella" value="Elimina">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+
+
+                                        {{-- <form method="POST" action="{{route('user.apartments.destroy', $apartment->id)}}">
     
                                             @csrf
                                             @method('DELETE')
                   
-                                            {{-- chiedere conferma della cancellazione, prima di eliminare l’elemento --}}
-                                            {{-- <a type="submit" onclick="return confirm('Sei sicuro?')"><img src="http://127.0.0.1:8000/storage/icons/normal/cancella.svg" alt="mostra" class="svg_dash"></a> --}}
-                                            <a type="submit" onclick="document.getElementById('btnDelete').style.display='block'"><img src="http://127.0.0.1:8000/storage/icons/normal/cancella.svg" alt="mostra" class="svg_dash"></a>
+                                            
+                                            <a onclick="document.getElementById('btnDelete').style.display='block'"><img src="http://127.0.0.1:8000/storage/icons/normal/cancella.svg" alt="mostra" class="svg_dash"></a>
 
                                             
-                                            {{-- <button onclick="document.getElementById('id01').style.display='block'"><img src="http://127.0.0.1:8000/storage/icons/normal/cancella.svg" alt="mostra" class="svg_dash"></button> --}}
+                                          
 
                                             <div id="btnDelete" class="modal">
                                                 <span onclick="document.getElementById('btnDelete').style.display='none'" class="close" title="Close Modal">×</span>
-                                                <form class="modal-content">
+                                                <div class="modal-content">
                                                     <div class="containerDeletebtn">
                                                         <h1>Cancella Appartamento</h1>
                                                         <p>Sei sicuro di voler eliminare questo appartamento?</p>
@@ -85,46 +129,13 @@
 
                                                         <div class="clearfix centro">
                                                             <button type="button" onclick="document.getElementById('btnDelete').style.display='none'" class="annulla">Annulla</button>
-                                                            <button type="submit" onclick="document.getElementById('btnDelete').style.display='none'" class="cancella" >Cancella</button>
+                                                            <button type="button" onclick="deleteThis(this.id)" class="cancella"  id="{{$apartment->id}}">Cancella</button>
                                                         </div>
 
                                                     </div>
-                                                </form>
-                                            </div>
-                                        </form>
-
-                                        <div id="showMessage" class="modal messages">
-                                            <span onclick="document.getElementById('showMessage').style.display='none'" class="exit" title="Close Modal">×</span>
-                                            <div class="holes"></div>
-                                            <div class="dashed"></div>
-
-                                                <div class="messagesBox">
-                                                    @foreach ($apartments as $apartment)
-                                                        @if (count($apartment->messages) != 0)
-                                                        
-                                                            <div class="singleApartmentMessages">
-                                                                <div class="text-left pl-2 mb-2">
-                                                                    <h3 class="title"><a href="{{route('user.apartments.show', $apartment->slug)}}">{{$apartment->title}}</a></h3>
-                                                                </div>
-                                                                @foreach ($apartment->messages as $message)
-                                                                    <div class="singleMessage">
-                                                                        <div class="info pl-2">
-                                                                            <div>{{$message->email}}</div>
-                                                                            <div class="timeStamp">{{$message->created_at}}</div>
-                                                                        </div>
-                                                                        <div class="contentMessage">
-                                                                            {{$message->content}}
-                                                                        </div>
-                                                                    </div>
-
-                                                                @endforeach
-                                                            </div>
-                                                        @endif
-
-                                                    @endforeach
                                                 </div>
-
-                                        </div>
+                                            </div>
+                                        </form> --}}
 
                                     </div>
                                 </td>
@@ -140,15 +151,7 @@
 
     
     <script>
-        // Get the modal
-        var modal = document.getElementById('btnDelete');
-        
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        }
+
     </script>
 
 @endsection
